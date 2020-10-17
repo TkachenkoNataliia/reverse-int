4d76a1c4e812fa7b030049371e48e90da65b9ff0
TkachenkoNataliia <nattaliiatkachenko@gmail.com>
2020-10-17 21:54:03 +0300

    feat: add function reverse(n)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n > 0) {
+        let str = n.toString();
+        let result = "";
+        for (let i = 0; i < str.length; i++) {
+            result = str[i] + result;
+        }
+        return result;
+    } else {
+        let newN = Math.abs(n);
+        let str = newN.toString();
+        let result = "";
+        for (let i = 0; i < str.length; i++) {
+            result = str[i] + result;
+        }
+        return result;
+    }
+};
